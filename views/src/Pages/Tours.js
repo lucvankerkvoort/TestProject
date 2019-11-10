a37aec3a6337a50b652c2a076348138b50409448
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-11-10 15:09:47 -0800

    finished up the mockup for the tours page

--- views/src/Pages/Tours.js
+++ views/src/Pages/Tours.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { guide } from "../Components/images";
+import { guide, traveler } from "../Components/images";
 import BackgroundSlideshow from "react-background-slideshow";
 class Tours extends React.Component {
   state = {
@@ -9,8 +9,35 @@ class Tours extends React.Component {
     return (
       <div className="tours">
         <div className="guide-background">
-          <BackgroundSlideshow images={guide} />
+          <BackgroundSlideshow
+            images={this.state.user === "Guide" ? guide : traveler}
+          />
         </div>
+        <div className="tours-welcome">
+          <h2>Here You Can Check Your Active Tours</h2>
+        </div>
+        <div className="tours-main">
+          <div className="main-filler-left" />
+          <div className="tours-section">
+            <div className="tour">
+              <div className="mini-profile"></div>
+              <p>Name</p>
+              <p>dates</p>
+            </div>
+            <div className="tour">
+              <div className="mini-profile"></div>
+              <p>Name</p>
+              <p>dates</p>
+            </div>
+            <div className="tour">
+              <div className="mini-profile"></div>
+              <p>Name</p>
+              <p>dates</p>
+            </div>
+          </div>
+          <div className="main-filler-right" />
+        </div>
+        <div className="tours-filler"></div>
       </div>
     );
   }
