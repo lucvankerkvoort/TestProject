b32627a20fb841cf42c93a545073e9edd11fcc02
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-11-10 15:25:15 -0800

    made both the  tour as the options pages mobile responsive

--- views/src/Pages/Options.js
+++ views/src/Pages/Options.js
@@ -11,6 +11,15 @@ class Options extends React.Component {
         <div className="guide-background">
           <BackgroundSlideshow images={guide} />
         </div>
+        <div className="options-welcome">
+          <h2>Here You Can Change Your Information</h2>
+        </div>
+        <div className="main-options">
+          <div className="main-filler-left" />
+          <div className="main-section"></div>
+          <div className="main-filler-right" />
+        </div>
+        <div className="options-filler" />
       </div>
     );
   }
