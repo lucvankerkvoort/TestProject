ac9cc71149ca87640c3a8552db98f055fd1b0f17
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-11-29 17:15:12 -0800

    made a start with the google maps API

---
+++ views/src/Components/GoogleMaps/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+
+class GoogleMaps extends React.Component {
+  render() {
+    return <h1>HelloWorld</h1>;
+  }
+}
+
+export default GoogleMaps;
